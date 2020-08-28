5d62ff2a6a6e6f9023e8b5b2d15c00a6f33dc40c
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-08-28 15:44:31 +0300

    Updates

---
+++ precache-manifest.58aaa49226f19372a07f63ee03fe3377.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76abb90ad27daefa56ab8b75a829bf0a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8996df5b05e3451ec704",
+    "url": "/contactmanager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4a9af2aa77e1d5d44f79",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8996df5b05e3451ec704",
+    "url": "/contactmanager/static/js/2.9256ba54.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.9256ba54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9af2aa77e1d5d44f79",
+    "url": "/contactmanager/static/js/main.7ee0b3a9.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
